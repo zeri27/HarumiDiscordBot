7b687aed95f4ff22cda0e11281a850e350853422
Zeryab Alam <zeryabalam272@gmail.com>
2023-02-28 14:54:00 +0100

    Implemented certain basic bot features.

--- src/botFunctions.py
+++ src/botFunctions.py
@@ -1,3 +1,76 @@
 import random
 import discord
 from googletrans import Translator
+
+translator = Translator()
+
+with open('resources/randomfacts', 'r') as f:
+    lines = [line.strip() for line in f.readlines()]
+
+with open('resources/quotes', encoding='utf-8') as q:
+    quotes = q.readlines()
+
+colors = {
+    'red': 0xff0000,
+    'green': 0x00ff00,
+    'blue': 0x0000ff,
+    'yellow': 0xffff00,
+    'orange': 0xffa500,
+    'purple': 0x800080,
+    'pink': 0xffc0cb,
+    'gray': 0x808080,
+    'white': 0xffffff,
+    'black': 0x000000
+}
+
+factDescription = ['Thrilling', 'Exhilarating', 'Stimulating', 'Electrifying', 'Rousing', 'Arousing', 'Captivating',
+                   'Engaging', 'Enthralling', 'Enchanting', 'Fascinating', 'Mesmerizing', 'Gripping', 'Compelling',
+                   'Riveting']
+
+quoteDescription = ['Motivating', 'Encouraging', 'Uplifting', 'Stimulating', 'Galvanizing', 'Enlivening',
+                    'Invigorating', 'Emboldening', 'Inspiriting', 'Electrifying', 'Empowering', 'Cheering',
+                    'Heartening', 'Refreshing', 'Activating']
+
+
+def randomFact():
+    random_fact = random.choice(lines)
+    factDesc = random.choice(factDescription)
+    embed = discord.Embed(title="Harumi's " + factDesc + " Fact", description=random_fact,
+                          color=colors['blue'])
+    return embed
+
+
+def randomQuote():
+    random_quote = random.choice(quotes)
+    quoteDesc = random.choice(quoteDescription)
+    embed = discord.Embed(title="Harumi's " + quoteDesc + " Quote", description=random_quote,
+                          color=colors['yellow'])
+    return embed
+
+
+def helpFunction():
+    embed = discord.Embed(title="Harumi Help", description="Hey darling, I'm Harumi",
+                          color=colors['black'])
+    embed.add_field(name="!help",
+                    value="You just used this. I can help you when you use this command",
+                    inline=False)
+    embed.add_field(name="!work",
+                    value="Let's work and make some sweet sweet cash", inline=False)
+    embed.add_field(name="!fact",
+                    value="You receive a random fact", inline=False)
+    embed.add_field(name="!quote",
+                    value="You receive a random quote", inline=False)
+    embed.add_field(name="!translate",
+                    value="I translate the message to English :)", inline=False)
+    embed.set_image(url="https://pbs.twimg.com/profile_images/1498079070227099649/-2NWkrq3_400x400.jpg")
+    return embed
+
+
+def translateToEnglish(text):
+    translation = translator.translate(text, dest='en')
+    embed = discord.Embed(title="Harumi's Translation To English", color=colors['orange'])
+    embed.add_field(name="Original Text:",
+                    value=text, inline=False)
+    embed.add_field(name="Translated Text:",
+                    value=translation.text, inline=False)
+    return embed
--- src/botMain.py
+++ src/botMain.py
@@ -1,4 +1,68 @@
 import asyncio
 
 import discord
-from discord import Game
+from discord import Game, Status
+
+import config
+from src.botFunctions import helpFunction, randomFact, randomQuote, translateToEnglish
+
+TOKEN = config.TOKEN
+
+intents = discord.Intents.all()
+client = discord.Client(intents=intents)
+
+userCoins = {"Jahy": 1000}
+
+
+@client.event
+async def on_ready():
+    await client.change_presence(activity=Game(name="!help ❤"), status=Status.dnd)
+    print('Logged in as {0.user}'.format(client))
+
+
+@client.event
+async def on_message(message):
+    print(str(message.author) + ": " + str(message.content))
+
+    if message.author == client.user:
+        return
+    else:
+        # Check if the message starts with '!'. Only check for content then.
+        if message.content.startswith('!'):
+            msg = message.content.lower()
+            # Help Function: Bot responds with embed that provides information on what the bot can do for each
+            # command entered
+            if msg.__eq__('!help'):
+                await message.reply(embed=helpFunction())
+            # Fact Function: Bot responds with a random fact
+            elif msg.__eq__('!fact'):
+                await message.reply(embed=randomFact())
+            # Quote Function: Bot responds with a random quote
+            elif msg.__eq__('!quote'):
+                await message.reply(embed=randomQuote())
+            # Translate Function: Bot translates the message that's been replied to and returns output in English
+            # Any language -> English (uses Google Translator API)
+            elif msg.__eq__('!translate'):
+                if message.reference:
+                    try:
+                        replied_message = await message.channel.fetch_message(message.reference.message_id)
+                        await message.reply(embed=translateToEnglish(replied_message.content))
+                    except discord.errors.NotFound:
+                        await message.channel.send("The replied message could not be found.")
+                else:
+                    await message.reply("Please reply to a message to translate it.")
+            # No proper command detected -> respond with generic supporting message
+            else:
+                await message.reply("I could not understand the command. \nPlease type ***!help*** to see everything I "
+                                    "can do and all the commands I can respond to.")
+
+
+async def run_bot():
+    try:
+        await client.start(TOKEN)
+    finally:
+        await client.close()
+
+
+loop = asyncio.get_event_loop()
+loop.run_until_complete(run_bot())
